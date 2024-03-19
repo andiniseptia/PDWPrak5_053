92697edfbbcf4324ca0fc0f365f0ce33287eb1ba
andiniseptia <andiniseptiandri124@gmail.com>
2024-03-19 09:51:09 +0700

    my first commit

---
+++ Praktikum3pdw/script.js
@@ -0,0 +1,15 @@
+function showAlert(){
+    alert(Date(),tampil(),show())
+    Date()
+
+    let tanggal = new Date();
+    document.getElementById("hasil").innerHTML = tanggal;
+
+    function tampil(){
+        alert("Tulip merupakan tumbuhan tahunan berumbi yang tingginya antara 10–70 cm, daunnya berlilin, berbentuk sempit memanjang berwarna hijau nuansa kebiru-biruan, dan bunganya berukuran besar terdiri 6 helai daun mahkota.")
+    }
+
+    function show(){
+        alert("Tulip adalah bunga yang melambangkan keindahan dan kesempurnaan. ")
+    }
+}
